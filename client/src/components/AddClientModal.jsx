4892fefacabbe2f4d408427668fbe4a406334796
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2023-02-11 14:18:54 +0200

    Add add client components

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -9,8 +9,30 @@ export default function AddClientModal() {
 	const [email, setEmail] = useState('');
 	const [phone, setPhone] = useState('');
 
+	const [addClient] = useMutation(ADD_CLIENT, {
+		variables: {name, email, phone},
+		update(cache, { data: { addClient } }) {
+			const { clients } = cache.readQuery({ query: GET_CLIENTS });
+
+			cache.writeQuery({
+				query: GET_CLIENTS,
+				data: { clients: [...clients, addClient] },
+			});
+		},
+	});
+
 	const onSubmit = (e) => {
 		e.preventDefault();
+
+		if (name === '' || email === '' || phone === '') {
+			return alert('Please fill in all fields');
+		}
+
+		addClient(name, email, phone);
+
+		setName('');
+		setEmail('');
+		setPhone('');
 	}
 
   return (
